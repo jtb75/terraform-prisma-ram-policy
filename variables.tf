04d4f5d95268145bc53da4fbdb8a323999c58346
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-29 13:09:02 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "apiurl" {
+    type = string
+    description = "API URL for Associated Prisma Cloud Console"
+}
+
+variable "pc_secret_key" {
+    type = string
+    description = "Prisma Cloud Secret Key"
+}
+
+variable "pc_access_key" {
+    type = string
+    description = "Prisma Cloud Access Key"
+}
