a419339a5931f1039ff3dbd28146a485eba2579d
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-29 14:02:58 -0500

    Update ram-policy.tf

--- ram-policy.tf
+++ ram-policy.tf
@@ -1,10 +1,10 @@
 resource "prismacloud_policy" "ram" {
-    name = "Alibaba Cloud RAM password retry constraint policy is disabled"
+    name = "AWS S3 buckets are accessible to public"
     policy_type = "config"
-    cloud_type = "alibaba_cloud"
+    cloud_type = "aws"
     rule {
-        name = "Alibaba Cloud RAM password retry constraint policy is disabled"
-        criteria = "9ec9839c-31b5-42f6-897d-ab13e9204db6"
+        name = "AWS S3 buckets are accessible to public"
+        criteria = "67dc23e7-e6db-4efb-b7e8-b4bd7778c891"
         parameters = {
             "savedSearch": "true",
             "withIac": "false",
