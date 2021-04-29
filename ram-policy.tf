14a03e68b1b18587d2c06e80cab73cda87e8175b
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-29 13:22:41 -0500

    Update ram-policy.tf

--- ram-policy.tf
+++ ram-policy.tf
@@ -1,7 +1,7 @@
 resource "prismacloud_policy" "ram" {
     name = "Alibaba Cloud RAM password retry constraint policy is disabled"
     policy_type = "config"
-    cloud_type = "alibaba"
+    cloud_type = "alibaba_cloud"
     rule {
         name = "Alibaba Cloud RAM password retry constraint policy is disabled"
         criteria = "9ec9839c-31b5-42f6-897d-ab13e9204db6"
